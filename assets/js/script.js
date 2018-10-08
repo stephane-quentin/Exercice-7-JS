90ac23bf7e421bd3905eedc84547de31b60d7990
Stiouf <stephane.quentin@live.fr>
2018-10-08 12:28:20 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 function ex7(){
   var shoe = document.getElementById("shoeSize").value; // récupération de la première variable que l'on a tapé dans la page html
   var year = document.getElementById("yearOfBirth").value;
-  var result = (((((shoe * 2) + 5) * 50) - year) + 1766); //soustraction multiplication addition ...
-  alert("Résultat : " + result);
+  var result = (((((shoe * 2) + 5) * 50) - year) + 1768); //soustraction multiplication addition ...
+
+  if(shoe=='' || year==''){
+    alert('Au moins un champ est vide')
+    }else{
+      if(isNaN(shoe) || isNaN(year)){ // OU logique
+        alert('Erreur!');
+      } else {
+        alert("Résultat : " + result);
+        }
+      }
 }
